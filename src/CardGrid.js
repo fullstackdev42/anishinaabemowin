da4cc278db9daa1f8b17e9cec8a0fabb5db8f955
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-10-01 12:07:37 -0400

    Remove unused png's

--- src/CardGrid.js
+++ src/CardGrid.js
@@ -7,53 +7,66 @@ export class CardGrid {
         this.config = gridConfiguration;
         this.CARD_WIDTH = 98;
         this.CARD_HEIGHT = 128;
-        this.TOTAL_PAIRS = 5;
         this.cards = [];
-        this.wordPairs = ["card-0", "card-1", "card-2", "card-3", "card-4", "card-5"];
+    }
+
+    calculateGridDimensions() {
+        const scaledCardWidth = this.CARD_WIDTH * this.config.cardScale;
+        const scaledCardHeight = this.CARD_HEIGHT * this.config.cardScale;
+        return {
+            gridWidth: (scaledCardWidth * this.config.columns) + (this.config.paddingX * (this.config.columns - 1)),
+            gridHeight: (scaledCardHeight * this.config.rows) + (this.config.paddingY * (this.config.rows - 1)),
+            scaledCardWidth,
+            scaledCardHeight
+        };
     }
 
     calculateGridPosition() {
-        const scaledCardWidth = this.CARD_WIDTH * this.config.cardScale;
-        const scaledCardHeight = this.CARD_HEIGHT * this.config.cardScale;
-        const gridWidth = (scaledCardWidth * this.config.columns) + (this.config.paddingX * (this.config.columns - 1));
-        const gridHeight = (scaledCardHeight * this.config.rows) + (this.config.paddingY * (this.config.rows - 1));
-        
-        // Center the grid horizontally and vertically
+        const { gridWidth, gridHeight } = this.calculateGridDimensions();
         this.config.x = (this.scene.sys.game.config.width - gridWidth) / 2;
         this.config.y = (this.scene.sys.game.config.height - gridHeight) / 2;
     }
 
+    getShuffledCardNames() {
+        const cardNames = this.scene.gameState.cardNames.concat(this.scene.gameState.cardNames);
+        return Phaser.Utils.Array.Shuffle(cardNames);
+    }
+
+    calculateCardPosition(row, col, scaledCardWidth, scaledCardHeight) {
+        const startX = this.config.x;
+        const x = startX + col * (scaledCardWidth + this.config.paddingX) + scaledCardWidth / 2;
+        const y = this.config.y + row * (scaledCardHeight + this.config.paddingY) + scaledCardHeight / 2;
+        return { x, y };
+    }
+
+    createCard(cardName, position) {
+        const card = createCard({
+            scene: this.scene,
+            x: position.x,
+            y: position.y,
+            cardText: cardName,
+            cardName: cardName
+        });
+        card.gameObject.setScale(this.config.cardScale);
+        return card;
+    }
+
     createGridCards() {
         this.calculateGridPosition();
-        const cardNames = this.scene.gameState.cardNames.concat(this.scene.gameState.cardNames);
-        Phaser.Utils.Array.Shuffle(cardNames);
-    
-        const scaledCardWidth = this.CARD_WIDTH * this.config.cardScale;
-        const scaledCardHeight = this.CARD_HEIGHT * this.config.cardScale;
-        const totalWidth = (this.config.columns * scaledCardWidth) + ((this.config.columns - 1) * this.config.paddingX);
-        const startX = (this.scene.sys.game.config.width - totalWidth) / 2;
-    
+        const cardNames = this.getShuffledCardNames();
+        const { scaledCardWidth, scaledCardHeight } = this.calculateGridDimensions();
+
         for (let row = 0; row < this.config.rows; row++) {
             for (let col = 0; col < this.config.columns; col++) {
                 const cardIndex = row * this.config.columns + col;
                 if (cardIndex < cardNames.length) {
-                    const x = startX + col * (scaledCardWidth + this.config.paddingX) + scaledCardWidth / 2;
-                    const y = this.config.y + row * (scaledCardHeight + this.config.paddingY) + scaledCardHeight / 2;
-    
-                    const card = createCard({
-                        scene: this.scene,
-                        x,
-                        y,
-                        cardText: cardNames[cardIndex],
-                        cardName: cardNames[cardIndex]
-                    });
-    
-                    card.gameObject.setScale(this.config.cardScale);
+                    const position = this.calculateCardPosition(row, col, scaledCardWidth, scaledCardHeight);
+                    const card = this.createCard(cardNames[cardIndex], position);
                     this.cards.push(card);
                 }
             }
         }
-    
+
         return this.cards;
     }
 
